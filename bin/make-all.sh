54c4c216eb83970e7e7388495925dfc49dc0dcb7
Bill Rees <bill@zglue.com>
2017-11-30 12:09:57 -0800

    Format the parsing while loop for help msg

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -5,33 +5,33 @@ ARG_LIST="BOARD TARGET_DIRS NO_ERRORS Q V VERBOSE RELOCATE"
 
 #
 # Filter out everything but the while loop and the two boundary markers.
-# Then filter out anything that does not have a '-' in it.  Voila, instant
+# Then filter out anything that does not have a ')' in it.  Voila, instant
 # help message!
 #
 help_msg()
 {
-        sed -n -e '/^### HELP.*start/,/^### HELP.*end/p' "$0" | grep "\-" | \
-                grep -v 'while'
+        sed -n -e '/^### HELP.*start/,/^### HELP.*end/p' "$0" | sed -n -e '/)/p'
+#       grep "\-" |  grep -v 'while'
 }
 
 ### HELP message start
 while [ $# -ne 0 ] ; do
         case "$1" in
-        -b) BOARD="$2"; shift   # Specify the directory to build from
+        -b) BOARD="$2"; shift              # Specify the directory to build from
                 ;;
-        -e) NO_ERRORS="NO_ERRORS=1" # Disable -Werror (warnings are errors)
+        -e) NO_ERRORS="NO_ERRORS=1"        # Disable -Werror (warnings are errors)
                 ;;
         -h) help_msg && exit 0
                 ;;
         -p) print_vars && exit 0
                 ;;
-        -Q) Q="";   # Expose the raw commands make issues.
+        -Q) Q="";                          # Expose the raw commands make issues.
                 ;;
-        -t) TARGET_DIRS="$TARGET_DIRS $2"; shift   # Specify build target.
+        -t) TARGET_DIRS="$TARGET_DIRS $2"; shift   # Specify build target. May specify multiple times.
                 ;;
-        -v) VERBOSE="${VERBOSE}V"   # Print info beyond success/failure.
+        -v) VERBOSE="${VERBOSE}V"          # Print info beyond success/failure. May specify multiple times.
                 ;;
-        -V) V=2         # Essentially a dup of -Q 
+        -V) V=2                            # Essentially a dup of -Q 
                 ;;
         *|--) echo "Unexpected argument. ($1)" >&2  #
                 exit 1
