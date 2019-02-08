2285dfb8b042cd3e9e1b852a584ce43b42072b80
Bill <bill@zglue.com>
2019-02-07 16:51:01 -0800

    Back off using os.popen for subprocesses
    Change-Id: Iae4e8c631cbe10d0fdc0f77187d501ad7bf42128

--- bin/style.py
+++ bin/style.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 
 import os
+CURDIR = "."
+## count = "100"
+## spot = "init"
+## cmd = "find "
+## cmd.join(spot)
+## # cmd.append( "-type f -name "*.c" -o -name "*.h" "|" )
+##
+## print("Set amount\n")
+## amount = int(count)
+## xx = type(amount)
+## xx
+##
+## print("Build command\n")
+## cmd.join( "-type f")
+## fd = os.popen(cmd, mode='r', buffering=1)
+## if not fd:
+##     print("failed to open the pipe: %", fd)
+## xx = type(fd.read)
+## print(";; fd \n", xx)
+## xx = type(amount)
+## print(":: \n", xx)
+##
+## os.read(fd.read, 100)
+##
 
-count = "100"
-spot = "init"
-cmd = "find "
-cmd.join(spot)
-# cmd.append( "-type f -name "*.c" -o -name "*.h" "|" )
+def list_files(DIR):
+    for Dir, Dirpath, File, in os.walk(DIR, topdown=True, followlinks=False):
+        print(Dirpath, "/", File, "\n")
 
-print("Set amount\n")
-amount = int(count)
-xx = type(amount)
-xx
 
-print("Build command\n")
-cmd.join( "-type f")
-fd = os.popen(cmd, mode='r', buffering=1)
-if not fd:
-    print("failed to open the pipe: %", fd)
-xx = type(fd)
-print(";; fd \h", xx)
-xx = type(amount)
-print(":: \n", xx)
-
-# buf = os.read(fd, 10)
-
+list_files(CURDIR)
 
