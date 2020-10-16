00bc9b2b0e7907d41f74c149f9605589a6707758
Bill Hooper <me@billrees.io>
2020-10-15 21:33:55 -0700

    Handle diff distros with diff terminal progs

--- bin/display.py
+++ bin/display.py
@@ -1,19 +1,57 @@
 #!/usr/bin/env python3
 
 '''
-    Called from autostart to make sure the display resolution is set to 2560x1600
+    Called from autostart to make sure the display resolution is set to 2560x1600,
+    and two terminal windows are up.
 '''
 
 import os
 import subprocess
 
-Action = ['xrandr', '--output', 'Virtual1', '--mode', '2560x1600']
-# Action = ['xrandr', '--output', 'Virtual1', '--mode', '800x600']
+XRANDR = "/usr/bin/xrandr"
+HOW_MANY_TERMS = 2
+TILIX = "/usr/bin/tilix"
+GNOME_TERMINAL = "/usr/bin/gnome-terminal"
 
-subprocess.run(Action)
+TASKS = dict()
+TASKS[XRANDR] = ['xrandr', '--output', 'Virtual1', '--mode', '2560x1600']
+TASKS[TILIX] = ['tilix', '--profile', "Light 0"]
+TASKS[GNOME_TERMINAL] = ['gnome-terminal', '--window-with-profile', "big an light"]
 
-PROFILE = "big an light"
-Action = ['gnome-terminal', '--window-with-profile', PROFILE]
-subprocess.run(Action)
-subprocess.run(Action)
+MORE_TASKS = dict()
+MORE_TASKS[TILIX] = ['tilix', '--profile', "Light 0"]
+MORE_TASKS[GNOME_TERMINAL] = ['gnome-terminal', '--window-with-profile', "big an light"]
 
+
+def is_there(thing):
+    '''
+        Run os.stat on the path and catch the ValueError exception
+        so that a typical return value is available.
+
+        Object is one of a file descriptor, file object, path.
+    '''
+    try:
+        # result = os.stat(thing)
+        os.stat(thing)
+    except ValueError:
+        return False
+    except FileNotFoundError:
+        return False
+    return True
+
+
+def run_tasks(da_tasks):
+    ''';
+        da_tasks a dictionary of file,command tuples.
+    '''
+
+    for items in da_tasks:
+        if is_there(items):
+            subprocess.run(da_tasks[items], check=True)
+        else:
+            return False
+        return True
+
+
+run_tasks(TASKS)
+run_tasks(MORE_TASKS)
