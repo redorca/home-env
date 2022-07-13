8f00ec143c4df43c003a73cf06ebd61db9b4570c
Bill Hooper <me@billrees.io>
2022-07-13 14:29:34 -0700

    cd to home before running the script

--- bin/gcloud.sh
+++ bin/gcloud.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cd $HOME
 sudo apt-get -y install apt-transport-https ca-certificates gnupg curl docker
 
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
