c54bd247c09f7774c02783dc87c4a59404bcaa6c
Bill Hooper <me@billrees.io>
2022-09-14 18:41:50 -0700

    cleaning up

--- bin/bill-dos-env.bat
+++
@@ -1,8 +0,0 @@
-
-set PATH=%HOMEDRIVE%%HOMEPATH%\bin;y:\bin;%PATH%
-
-net use y: /delete
-net use y: \\192.168.168.109\home
-
-cmd /k cd y:
-
--- bin/ubuntu-upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
-
-apt-get update && sudo apt-get dist-upgrade
-
-do-release-upgrade
-
