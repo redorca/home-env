a8309005f3e4e11e0ae6c40d6652ba559276b95c
Bill Rees <brees@dustyrobotics.com>
2022-10-03 17:08:51 -0700

    Add .config/git/ to setup coverage

--- bin/constants/constants.py
+++ bin/constants/constants.py
@@ -7,3 +7,7 @@ LOCAL = ".local"
 LOCO_BIN = "/".join([LOCAL, "bin"])
 CLONE_URI = "ssh://git@github.com/redorca/home-env.git"
 GIT_DIR = "bin/home-env/.git"
+SAMBA_ENTRY = [ "[]", "path = /home//", "read only = no", "browseable = yes", "guest ok = yes" ]
+SAMBA_GLOBAL = [ "guest account = USER" ]
+print(SAMBA_ENTRY[1])
+
