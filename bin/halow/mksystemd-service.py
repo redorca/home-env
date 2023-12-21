2a45754fc4a87642c8a2491bf824a59b7d658485
Bill Rees <uberfoot@yahoo.com>
2023-12-20 23:02:20 -0800

    Clean up vestigial xml strings

--- bin/halow/mksystemd-service.py
+++ bin/halow/mksystemd-service.py
@@ -7,20 +7,7 @@ import xml.etree.ElementTree as eltree
 
 TOP_TEMPLATE = ["<?xml version='1.0' standalone=\'no\'?><!--*-nxml-*-->",
                 "<!DOCTYPE service-group SYSTEM 'avahi-service.dtd'>"]
-XML_TEMPLATE = "<service-group>     \
-        \
-  <name replace-wildcards='yes'>Features on %h</name>       \
-        \
-  <service protocol='ipv4'>     \
-    <type>_http._tcp</type>     \
-    <port>5555</port>       \
-    <txt-record value-format='text'>key='blah blah blah'</txt-record>       \
-</service>      \
-</service-group>        \
-"
-GROUP_ELEMENTS = {'service':{'protocol':'ipv4'}, 'name':{'replace-wildcards':'yes', 'text':'stuff on %h'}}
-SERVICE_ELEMENTS = { 'host-name':None, 'type':'_http._tcp', 'port':5555}
-ROOT_ELEMENT={'root':'service-group', 'children':GROUP_ELEMENTS}
+
 def build_xmltree(xmlFile):
 	top = eltree.Element('service-group')
 	name = eltree.SubElement(top, 'name', attrib={"replace-wildcards":'yes'}).text = "Stuff on %h"
