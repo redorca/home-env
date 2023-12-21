88eabc06376664db7ec62500d9a99358f201f25d
Bill Rees <uberfoot@yahoo.com>
2023-12-20 16:34:59 -0800

    Convert hard-coded data to f'{vars}'

--- bin/config-mesh.py
+++ bin/config-mesh.py
@@ -8,21 +8,27 @@
 import subprocess as subp
 import os
 DEF_FLAGS = {'capture_output':True, "check":True}
-CMDS = [ "iw dev wlan1 interface add mesh0 type mp mesh_id meshme",
-        "iw dev mesh0 set channel 161",
-        "ifconfig wlan1 down",
-        "ifconfig mesh0 up",
-        "ip addr add 192.168.1.200/24 dev mesh0"]
 
 def node_create(channel, mesh_id, ipaddr):
+    '''
+    Fill out the command set with the values passed in.
+    '''
+    iface="mesh0"
+    wlan="wlan1"
+    CMDS = [ f"iw dev {wlan} interface add {iface} type mp mesh_id {mesh_id}",
+        f"iw dev {iface} set channel {channel}",
+        f"ifconfig {wlan} down",
+        f"ifconfig {iface} up",
+        f"ip addr add {ipaddr}/24 dev {iface}"]
     for index in range(len(CMDS)):
         cmd_args = ['sudo', *CMDS[index].split(' ')]
+        # print(f"[{index}]: {' '.join(cmd_args)}")
         try:
             result = subp.run(cmd_args, **DEF_FLAGS)
-            print(f'result: {result.stdout}')
+            print(f'{result.stdout}')
         except subp.CalledProcessError as CPE:
             print(f"Command::\n\t{' '.join(CPE.cmd)}\nfailed with error: {CPE.returncode}")
-            print(f'{CPE.stderr}')
+            print(f"{CPE.stderr.decode('utf8')}")
             return
 
 node_create(161, "meshme", "192.168.183.44")
