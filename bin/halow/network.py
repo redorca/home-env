34fb2f73016ee5f6d302177f2491a1595130998d
Bill Rees <uberfoot@yahoo.com>
2024-01-03 19:07:45 -0800

    classify mesh for each node.

--- bin/halow/network.py
+++ bin/halow/network.py
@@ -1,7 +1,7 @@
 '''
     Setup a mesh node given arguments provided.
     ipaddr     ipv4 address for the mesh interface
-    iface      The interface to operate on. e.g wlan0, wlan1
+    mesh      The interface to operate on. e.g wlan0, wlan1
     mesh_id    The equivalent to the ssid.
 '''
 
@@ -9,31 +9,53 @@ import subprocess as subp
 import os
 DEF_FLAGS = {'capture_output':True, "check":True}
 
-def node_create(channel, mesh_id, ipaddr):
+'''
+class node():
+    def __init__(self):
+        self
+'''
+
+
+class mesh():
+    def __init__(self, wlan, mesh, mesh_name, channel, ipaddr):
+        self.wlan = wlan
+        self.mesh = mesh
+        self.channel = channel
+        self.ipaddr = ipaddr
+        self.mesh_name = mesh_name
+
+    def node_create(self):
+        '''
+        Setup one mesh node on wlanX based on dynamic values.
+        Fill out the command set with the values passed in. The substition only works
+        within the local scope so the commands must be assigned in that scope
+        '''
+        CMDS = [ f"iw dev {self.wlan} interface add {self.mesh} type mp mesh_id {self.mesh_name}",
+                f"iw dev {self.mesh} set channel {self.channel}",
+                f"ifconfig {self.wlan} down",
+                f"ifconfig {self.mesh} up",
+                f"ip addr add {self.ipaddr}/24 dev {self.mesh}"]
+        for index in range(len(CMDS)):
+            cmd_args = ['sudo', *CMDS[index].split(' ')]
+            try:
+                result = subp.run(cmd_args, **DEF_FLAGS)
+                print(f'{result.stdout.decode("utf8")}')
+            except subp.CalledProcessError as CPE:
+                print(f"Command::\n\t{' '.join(CPE.cmd)}\nfailed with error: {CPE.returncode}")
+                print(f"{CPE.stderr.decode('utf8')}")
+                return
+
+def setup_link_local(interface, iftype):
     '''
-    Setup one mesh node on wlanX based on dynamic values.
+    interface is the name of the interface like eth0 or wlan5
+    iftype is the type of the interface; i.e. ethernet, wifi, olpc-mesh (olpc:: one laptop per child)
     '''
-    iface="mesh0"
-    wlan="wlan1"
-    '''
-    Fill out the command set with the values passed in. The substition only works
-    within the local scope so the commands must be assigned in that scope
-    '''
-    CMDS = [ f"iw dev {wlan} interface add {iface} type mp mesh_id {mesh_id}",
-        f"iw dev {iface} set channel {channel}",
-        f"ifconfig {wlan} down",
-        f"ifconfig {iface} up",
-        f"ip addr add {ipaddr}/24 dev {iface}"]
-    for index in range(len(CMDS)):
-        cmd_args = ['sudo', *CMDS[index].split(' ')]
-        try:
-            result = subp.run(cmd_args, **DEF_FLAGS)
-            print(f'{result.stdout}')
-        except subp.CalledProcessError as CPE:
-            print(f"Command::\n\t{' '.join(CPE.cmd)}\nfailed with error: {CPE.returncode}")
-            print(f"{CPE.stderr.decode('utf8')}")
-            return
+    CMD = f'nmcli c a ifname {interface} type {iftype} ipv4.method link-local ipv6.method disabled'
+    cmd_args = CMD.split(' ')
+    # result = subp.run(cmd_args, **DEF_FLAGS)
+    print(f'{cmd_args}')
 
 if __name__ == '__main__':
-    node_create(161, "meshme", "192.168.183.44")
+    setup_link_local('mesh0', 'wifi')
+    mesh('wlan0', 'mesh0', 'meshme', 161, "192.168.183.44").node_create()
 
