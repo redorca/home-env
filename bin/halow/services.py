a7d6cf846f9e6571587792562039f07564b60233
Bill Rees <uberfoot@yahoo.com>
2024-01-03 19:45:57 -0800

    Generalize service handling and services
      supercede mksystemd-service

---
+++ bin/halow/services.py
@@ -0,0 +1,10 @@
+'''
+handle services management and creation. Which means using systemctl, avahi service xml snippets, services.
+'''
+
+import subprocess as subp
+import os
+
+
+if __name__ == "__main__":
+    print("foo faa")
