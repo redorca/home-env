48ad9357b444c5b9169190e6edb9d9d0509f3f46
Bill Rees <Bill Rees>
2024-07-14 16:12:05 -0700

    look for colors.<hostname> rather than colors.host

--- bin/term-color.py
+++ bin/term-color.py
@@ -1,18 +1,24 @@
 #!/bin/env python3
 
 import configparser as parser
+import platform
 import os
 
 RC              =       0
 COLOR_CFILE     =      "Documents/colors.host"
 
+host = platform.node()
+file_name = '.'.join(['colors', host.lower()])
+print(f'Looking for Docuents/{file_name}')
+path = '/'.join(['Documents', file_name])
+print(f'path to look into {path}')
 kolors = parser.ConfigParser()
-kolors.read(COLOR_CFILE)
-print(kolors.sections())
+kolors.read(path)
+# print(kolors.sections())
 # print(kolors.items('default'))
 # print(kolors.items('types'))
-VAL=kolors.get('types', 'ARCHIVE')
-print("Value of ARCHIVE is ", VAL)
-VAL=kolors.get('default', 'GREEN')
-print("which is ", VAL)
+# VAL=kolors.get('types', 'ARCHIVE')
+# print("Value of ARCHIVE is ", VAL)
+# VAL=kolors.get('default', 'GREEN')
+# print("which is ", VAL)
 # kolors.getint('default,' VAL)
